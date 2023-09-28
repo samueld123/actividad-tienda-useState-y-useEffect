cfe3938418fe6d4721243a2ea2c91c36f3c395b7
SamuelDQS <138792093+SamuelDQS@users.noreply.github.com>
2023-09-28 11:06:33 -0500

    Funcionalidad de carrito completa
    Se incluyó agregar y quitar elementos del carrito desde el preview y desde el carrito

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,18 +1,114 @@
 import "./Cart.css";
 import { useEffect, useState } from "react";
+import { FaPlus, FaMinus } from "react-icons/fa6";
 
 function Cart() {
-  const [user, setUser] = useState([]);
+  const [cartItems, setCartItems] = useState({});
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/users/9")
-      .then((response) => response.json())
-      .then((data) => setUser(data));
+    const savedCart = JSON.parse(localStorage.getItem("cart")) || {};
+    setCartItems(savedCart);
   }, []);
+
+  const handleClickSubstract = (product) => {
+    const existingCart = JSON.parse(localStorage.getItem("cart")) || {};
+    if (existingCart[product.item.id].quantity > 1) {
+      existingCart[product.item.id].quantity -= 1;
+    } else {
+      delete existingCart[product.item.id];
+    }
+    localStorage.setItem("cart", JSON.stringify(existingCart));
+    setCartItems(existingCart);
+  };
+
+  const handleClickAdd = (product) => {
+    const existingCart = JSON.parse(localStorage.getItem("cart")) || {};
+    existingCart[product.item.id].quantity += 1;
+    localStorage.setItem("cart", JSON.stringify(existingCart));
+    setCartItems(existingCart);
+  };
 
   return (
     <>
-    <p>asdf</p>
+      <div className="cart">
+        {Object.keys(cartItems).length > 0 ? (
+          <>
+            <div className="cart-list">
+              <h1>Carrito de compras</h1>
+              <ul>
+                {Object.values(cartItems).map((product) => (
+                  <li key={product.item.id}>
+                    <div className="cart-item">
+                      <img
+                        className="cart-item-img"
+                        src={product.item.image}
+                        alt={product.item.title}
+                      />
+                      <div className="cart-item-details">
+                        <h4>{product.item.title}</h4>
+                        <h3>{product.item.category}</h3>
+                        <p>Cantidad: {product.quantity}</p>
+                        <p>Precio unitario: $ {product.item.price}</p>
+                        <p>
+                          Precio total:{" "}
+                          <strong>
+                            $ {product.item.price * product.quantity}
+                          </strong>
+                        </p>
+                        <div className="buttons-field">
+                          <button
+                            className="operation-buttons"
+                            onClick={() => handleClickSubstract(product)}
+                          >
+                            <FaMinus size={15} color="red" />
+                          </button>
+                          <button
+                            className="operation-buttons"
+                            onClick={() => handleClickAdd(product)}
+                          >
+                            <FaPlus size={15} color="green" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="cart-checkout">
+              <h1>Resumen</h1>
+              <div className="cart-checkout-card">
+                <div className="cart-checkout-card-line">
+                  <h4>Cantidad de productos: </h4>
+                  <p>
+                    {Object.values(cartItems).reduce(
+                      (total, product) => total + product.quantity,
+                      0
+                    )}
+                  </p>
+                </div>
+                <div className="cart-checkout-card-line">
+                  <h4>Precio total: </h4>
+                  <p>
+                    <strong>
+                      ${" "}
+                      {Object.values(cartItems).reduce(
+                        (total, product) =>
+                          total + product.item.price * product.quantity,
+                        0
+                      )}
+                    </strong>
+                  </p>
+                </div>
+                <hr />
+                <button className="cart-checkout-pay">Pagar</button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <h1>El carrito está vacío</h1>
+        )}
+      </div>
     </>
   );
 }
--- src/Components/ProductPreview/ProductPreview.jsx
+++ src/Components/ProductPreview/ProductPreview.jsx
@@ -1,17 +1,58 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import "./ProductPreview.css";
+import { FaPlus, FaMinus } from "react-icons/fa6";
 
 function Product() {
   const [product, setProduct] = useState([]);
+  const [cartItems, setCartItems] = useState({});
   const { id } = useParams();
-  console.log("id-producto", id);
 
   useEffect(() => {
     fetch(`https://fakestoreapi.com/products/${id}`)
       .then((response) => response.json())
       .then((data) => setProduct(data));
   }, []);
+
+  useEffect(() => {
+    const savedCart = JSON.parse(localStorage.getItem("cart")) || {};
+    setCartItems(savedCart);
+  }, []);
+
+  const handleClickSubstract = (product) => {
+    const existingCart = JSON.parse(localStorage.getItem("cart")) || {};
+    console.log("hola");
+    if (existingCart[product.id]) {
+      if (existingCart[product.id].quantity >= 1) {
+        existingCart[product.id].quantity -= 1;
+        console.log("2", existingCart[product.id].quantity);
+        if (existingCart[product.id].quantity === 0) {
+          delete existingCart[product.id];
+        }
+      }
+    }
+
+    console.log(existingCart);
+    localStorage.setItem("cart", JSON.stringify(existingCart));
+    setCartItems(existingCart);
+  };
+
+  const handleClickAdd = (product) => {
+    const existingCart = JSON.parse(localStorage.getItem("cart")) || {};
+    console.log("hola");
+    if (existingCart[product.id]) {
+      existingCart[product.id].quantity += 1;
+    } else {
+      existingCart[product.id] = {
+        item: product,
+        quantity: 1,
+      };
+    }
+    console.log(existingCart);
+    localStorage.setItem("cart", JSON.stringify(existingCart));
+    setCartItems(existingCart);
+  };
+
   return (
     <div className="product-preview">
       <Link to={"/"}>
@@ -19,17 +60,37 @@ function Product() {
       </Link>
       {product ? (
         <div className="preview-card">
-          <img className = "preview-img" src={product.image} alt={product.category} />
+          <img
+            className="preview-img"
+            src={product.image}
+            alt={product.category}
+          />
           <div className="preview-card-text">
             <h2 className="preview-card-titles">{product.title}</h2>
             <h3>{product.category}</h3>
             <h2 className="preview-card-titles">$ {product.price}</h2>
             <p className="preview-card-description">{product.description}</p>
-            <button>Agregar al carrito</button>
+            <div className="buttons-field">
+              <button
+                className="operation-buttons"
+                onClick={() => handleClickSubstract(product)}
+              >
+                <FaMinus size={15} color="red" />
+              </button>
+              <h3>
+                {cartItems[product.id] ? cartItems[product.id].quantity : 0}
+              </h3>
+              <button
+                className="operation-buttons"
+                onClick={() => handleClickAdd(product)}
+              >
+                <FaPlus size={15} color="green" />
+              </button>
+            </div>
           </div>
         </div>
       ) : (
-        <div> loading... </div>
+        <div> Cargando... </div>
       )}
     </div>
   );
